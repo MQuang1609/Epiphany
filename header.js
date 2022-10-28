958aa8d7330be0c2793afbf46f66b7b69e3329b0
minh quang <lunganh31@gmail.com>
2022-10-28 18:27:06 +0700

    Minh Quang

--- header.js
+++ header.js
@@ -2,6 +2,37 @@ const account = document.querySelector('#account');
 const headerSub = document.querySelector('.header-sub');
 
 account.addEventListener('click', () => {
-    console.log('abc');
-    headerSub.classList.toggle('active');
-})đâsda
+    headerSub.classList.toggle('active_1');
+})
+
+const btn = document.querySelectorAll('.header-list-wrapper button');
+
+
+// function activeContentTab(target) {
+//     const tabActiveElement = document.querySelector('.product.active');
+//     tabActiveElement.classList.remove('active');
+//     const activetab = document.querySelector(target);
+//     activetab.classList.add('active');
+// }
+
+function handleChangeTab(btn) {
+    const tabActiveElement = document.querySelector('.products');
+    // const featured = document.querySelector('.featured');
+    tabActiveElement.classList.toggle('test');
+    // featured.classList.toggle('test');
+    const activeElement = document.querySelector('.active1');
+    console.log(activeElement);
+    activeElement.classList.remove('active1');
+    const addActiveElement = document.querySelector(btn.getAttribute('data-TabId'));
+    addActiveElement.classList.add('active1');
+    // headerContent.classList.toggle('test');
+
+
+}
+
+for (const tabBtn of btn) {
+    tabBtn.addEventListener('click', () => {
+        handleChangeTab(tabBtn);
+    })
+}
+
